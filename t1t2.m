0897ea28cf2544c906972ad4d06b6dd98d93e584
nobody <nobody@5cc97c55-a587-425c-8f15-b4cee860df3f>
2010-02-18 12:18:39 +0000

    Tweaking

--- t1t2.m
+++ t1t2.m
@@ -1,7 +1,7 @@
 % t1t2.m
 %
 %        $Id:$ 
-%      usage: t1t2(t1_fidname,t2_fidname);
+%      usage: t1t2(t1_fidname,t2_fidname,<verbose>,<T1T2alignment>);
 %         by: justin gardner
 %       date: 02/18/10
 %    purpose: does t1/t2 correction saves out a file called t1t2.hdr when everything is done.
@@ -41,14 +41,17 @@ getArgs(varargin,{'verbose=1','T1T2alignment=1'});
 if ~checkCommands(verbose),return,end
 
 % check filenames
-if ~checkFilenames(t1_fidname,verbose),return,end
-if ~checkFilenames(t2_fidname,verbose),return,end
+[tf t1_fidname] = checkFilenames(t1_fidname,verbose);
+if ~tf,return,end
+[tf t2_fidname] = checkFilenames(t2_fidname,verbose);
+if ~tf,return,end
 
 % load t1 files, align images and avergae
 t1 = loadImageFiles(t1_fidname,verbose);
 if isempty(t1),return,end
 t1 = alignImages(t1,verbose);
 t1 = averageImages(t1,verbose);
+%t1ic = t1t2IntensityContrastCorrection(t1,verbose);
 
 % load t2 files, align images and average, then align to T1
 t2 = loadImageFiles(t2_fidname,verbose);
@@ -57,15 +60,18 @@ t2 = averageImages(t2,verbose);
 if T1T2alignment
   t2 = alignT1toT2(t1,t2,verbose);
 end
+t2 = blurImage(t2,verbose);
 
 % now set threshold and divide T1 by T2
-vol = divideT1byT2(t1,t2);
-saveNifti(vol,'t1t2');
+if ~isempty(t2)
+  vol = divideT1byT2(t1,t2);
+  if ~isempty(vol),saveNifti(vol,'t1t2');end
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%
 %    checkFilenames    %
 %%%%%%%%%%%%%%%%%%%%%%%
-function tf = checkFilenames(filenames,verbose)
+function [tf filenames] = checkFilenames(filenames,verbose)
 
 % default return
 tf = 0;
@@ -76,8 +82,11 @@ filenames = cellArray(filenames);
 % check for proper filenames
 for i = 1:length(filenames)
   %  check if it is an hdr
-  filenames{i} = setext(filenames{i},'hdr',0);
-  if isfile(filenames{i}),continue,end
+  filename = setext(filenames{i},'hdr',0);
+  if isfile(filename)
+    filenames{i} = filename;
+    continue
+  end
   % check if it is a fid
   filenames{i} = setext(filenames{i},'fid',0);
   if ~isdir(filenames{i})
@@ -118,6 +127,21 @@ for i = 1:length(filenames)
   end
 end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%   intensityCorrection   %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function im = t1t2IntensityContrastCorrection(im,verbose)
+
+% get crop region
+if ~isfield(im,'crop')
+  im.crop = selectCropRegion(im.d);
+  drawnow;
+end
+if verbose,disppercent(-inf,'(t1t2) intensityContrastCorrection');end
+im.d = intensityContrastCorrection(im.d,im.crop);
+if verbose,disppercent(inf);end
+
+
 %%%%%%%%%%%%%%%%%%%%%
 %    alignImages    %
 %%%%%%%%%%%%%%%%%%%%%
@@ -138,7 +162,7 @@ Minitial = eye(4);
 rotFlag = 1;
 robust = 0;
 phaseFlag = 0;
-crop = selectCropRegion(im{1}.d);
+im{baseImage}.crop = selectCropRegion(im{1}.d);
 drawnow;
 
 % parameters for warping
@@ -150,7 +174,7 @@ interpMethod = 'linear';
 disppercent(-inf,'Aligning volumes');
 for i = restOfImages
   % compute motion comp parameters
-  M = estMotionIter3(im{baseImage}.d,im{i}.d,niters,Minitial,rotFlag,robust,phaseFlag,crop);
+  M = estMotionIter3(im{baseImage}.d,im{i}.d,niters,Minitial,rotFlag,robust,phaseFlag,im{baseImage}.crop);
   % display parameters
   if verbose
     disp(sprintf('(t1t2) Translating volume %s by [%s] to align to %s',im{i}.filename,mynum2str(M(1:3,4)),im{baseImage}.filename));
@@ -193,9 +217,9 @@ function t2 = alignT1toT2(t1,t2,verbose)
 AFNIcommand = '3dAllineate';
 
 % filenames
-t1filename = 't1_aligned_averaged.hdr';
-t2filename = 't2_aligned_averaged.hdr';
-t2alignedFilename = 't2_aligned_to_t1.hdr';
+t1filename = 't1.hdr';
+t2filename = 't2_preAlignment.hdr';
+t2alignedFilename = 't2.hdr';
 
 % write the files out to disk
 saveNifti(t1,t1filename);
@@ -203,18 +227,16 @@ saveNifti(t2,t2filename);
 
 % run the afni command
 disppercent(-inf,sprintf('(t1t2) Aligning T1 to T2 using AFNI:%s',AFNIcommand));
-system(sprintf('%s -verb -warp shift_rotate -base %s -source %s -prefix %s',AFNIcommand,t2filename,t1filename,t2alignedFilename));
+system(sprintf('%s -verb -warp shift_rotate -base %s -source %s -prefix %s',AFNIcommand,t1filename,t2filename,t2alignedFilename));
 disppercent(inf);
 
 % read back in aligned volume
 [t2.d t2.hdr] = cbiReadNifti(t2alignedFilename);
 
 % remove temporary files
-if 0
-  delete(setext(t1filename,'*'));
-  delete(setext(t2filename,'*'));
-  delete(setext(t2alignedFilename,'*'));
-end
+%  delete(setext(t1filename,'*'));
+%  delete(setext(t2filename,'*'));
+%  delete(setext(t2alignedFilename,'*'));
 
 %%%%%%%%%%%%%%%%%%%
 %    saveNifti    %
@@ -266,12 +288,14 @@ function vol = divideT1byT2(t1,t2,verbose)
 
 vol = [];
 
+% some global variables for displaying
+global t1t2fig;
+
 % display figure
 t1t2fig.f = smartfig('t1t2');
 t1t2fig.f2 = smartfig('t1t2final');
 
-% some global variables for displaying
-global t1t2fig;
+
 t1t2fig.t1 = t1;
 t1t2fig.t2 = t2;
 
@@ -279,9 +303,10 @@ maxSlice = size(t1.d,3);
 midSlice = round(maxSlice/2);
 
 % set up params
-paramsInfo{1} = {'threshold',1,'numeric=1','incdec=[-0.1 0.1]','minmax=[0 inf]','callback',@displayT1T2,'passParams=1'};
-paramsInfo{end+1} = {'sliceNum',midSlice,'numeric=1','incdec=[-1 1]',sprintf('minmax=[1 %i]',maxSlice),'callback',@displayT1T2,'passParams=1'};
-paramsInfo{end+1} = {'gamma',0.4,'numeric=1','incdec=[-0.1 0.1]','minmax=[0 inf]','callback',@displayT1T2,'passParams=1'};
+paramsInfo{1} = {'threshold',1,'numeric=1','incdec=[-0.1 0.1]','minmax=[0 inf]','callback',@displayT1T2,'passParams=1','Controls the threshold of the T2 image that creates the mask. Set this to make as clean a mask as possible'};
+paramsInfo{end+1} = {'sliceNum',midSlice,'numeric=1','incdec=[-1 1]',sprintf('minmax=[1 %i]',maxSlice),'callback',@displayT1T2,'passParams=1','Which slice to display'};
+paramsInfo{end+1} = {'blurLevel',t2.blurLevel,'numeric=1','incdec=[-1 1]','minmax=[0 inf]','round=1','callback',@displayT1T2,'passParams=1','Which slice to display'};
+paramsInfo{end+1} = {'gamma',0.4,'numeric=1','incdec=[-0.1 0.1]','minmax=[0 inf]','callback',@displayT1T2,'passParams=1','Changes the display gamma of the T1T2 image'};
 
 		
 % get default params to display initial image
@@ -290,20 +315,40 @@ displayT1T2(params);
 
 % now put up dialog box 
 params = mrParamsDialog(paramsInfo,'Set threshold');
-if isempty(params),return,end
 
 % clean up
 close(t1t2fig.f);
+close(t1t2fig.f2);
+drawnow;
 clear global t1t2fig;
 
-% and do the division
-mask = t2.d>params.threshold;
-mask = mask.*t2.d;
-mask(mask==0) = 1;
-vol.d = t1.d./mask;
+% user cancel
+if isempty(params),return,end
 
-vol.hdr = t1.hdr;
+vol = computeT1T2(t1,t2,params);
 
+
+%%%%%%%%%%%%%%%%%%%%%
+%%   computeT1T2   %%
+%%%%%%%%%%%%%%%%%%%%%
+function [t1t2 mask t2] = computeT1T2(t1,t2,params,sliceNum)
+
+% see if we have to blur T2
+if params.blurLevel ~= t2.blurLevel
+  t2 = blurImage(t2,1,params.blurLevel);
+end
+
+% default to all slices
+if ieNotDefined('sliceNum'),sliceNum = 1:size(t1,3);end
+
+% get mask
+mask = t2.d(:,:,sliceNum)>params.threshold;
+divmask = mask.*t2.blurd(:,:,sliceNum);
+divmask(divmask==0) = inf;
+
+% do the division
+t1t2.d = t1.d(:,:,sliceNum)./divmask;
+t1t2.hdr = t1.hdr;
 
 %%%%%%%%%%%%%%%%%%%%
 %    displayT1T2   %
@@ -312,32 +357,68 @@ function displayT1T2(params)
 
 global t1t2fig;
 
+% display T1
 figure(t1t2fig.f);
-
 subplot(1,3,1);
 imagesc(t1t2fig.t1.d(:,:,params.sliceNum));
 axis square;
 title('T1');
+drawnow
 
+% compute division
+if ~isfield(t1t2fig,'params') || (t1t2fig.params.threshold ~= params.threshold) || (t1t2fig.params.blurLevel ~= params.blurLevel) || (t1t2fig.params.sliceNum ~= params.sliceNum)
+  [t1t2fig.t1t2 t1t2fig.mask t1t2fig.t2] = computeT1T2(t1t2fig.t1,t1t2fig.t2,params,params.sliceNum);
+end
+
+% display T2
+figure(t1t2fig.f);
 subplot(1,3,2);
-imagesc(t1t2fig.t2.d(:,:,params.sliceNum));
+imagesc(t1t2fig.t2.blurd(:,:,params.sliceNum));
 axis square;
 title('T2');
 
+% display mask
 subplot(1,3,3);
-mask = t1t2fig.t2.d(:,:,params.sliceNum)>params.threshold;
-imagesc(mask);
+figure(t1t2fig.f);
+imagesc(t1t2fig.mask);
 axis square;
 title('Mask');
 
+% set colormap
 colormap(gray);
 
 % draw t1/t2
 figure(t1t2fig.f2);
-mask = mask.*t1t2fig.t2.d(:,:,params.sliceNum);
-mask(mask==0) = 1;
-imageg(t1t2fig.t1.d(:,:,params.sliceNum)./mask,params.gamma);
+imageg(t1t2fig.t1t2.d,params.gamma);
 title('T1/T2');
 
 colormap(gray);
+
+%%%%%%%%%%%%%%%%%%%
+%%   blurImage   %%
+%%%%%%%%%%%%%%%%%%%
+function img = blurImage(img,verbose,nIter)
+
+% nIter and blurLevel are same thing
+if ieNotDefined('nIter'),nIter = 1;end
+img.blurLevel = nIter;
+
+% no bluring
+if nIter == 0
+  img.blurd = img.d;
+  return
+end
+
+% pre allocate memory
+if ~isfield(img,'blurd')
+  img.blurd = zeros(size(img.d));
+end
+
+% do bluring
+if verbose,disppercent(-inf,'(t1t2) Blurring T2 image');end
+for i = 1:size(img.d,3)
+  img.blurd(:,:,i) = blur(img.d(:,:,i),nIter);
+  if verbose,disppercent(i/size(img.d,3));end
+end
+if verbose,disppercent(inf);end
 
