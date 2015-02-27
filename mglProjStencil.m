50bc2025398bf360e0b2e056377418b152600a58
dbirman <danbirman@gmail.com>
2015-02-27 13:57:37 -0800

    Updated mglProjStencil
    Correct eccentricity values for projector screen with 32ch head coil.
    Avoids interacting with other stencils
    Flipped angles correctly

--- mglProjStencil.m
+++ mglProjStencil.m
@@ -1,18 +1,18 @@
-function stencil = mglProjStencil(eccArray)
+function stencil = mglProjStencil()
 %MGLPROJSTENCIL Generate a mask stencil for the GE projecter @ Stanford
 %   Builds a stencil based on the input list, which corresponds to the
 %   visual eccentricity at angles [90, -90, 0, 15, 30, 45, 60, 75, -15, -30, -45, -60, -75]
 % use by adding mglStencilSelect(1); and then mglStencilSelect(0); to turn
 % off
 
-% eccArray = [15, 13, 10, 8 , 5, 4.5, 14, 12, 10, 9, 8, 7 5];
+eccArray = [5.5, 7.5, 17, 18, 19, 11, 8, 6,14 , 15, 11, 8, 7.5];
 
-stencil = 1;
+stencil = 99;
 
 angles = [90, -90, 0, 15, 30, 45, 60, 75, -15, -30, -45, -60, -75];
 angles2 = [180-angles(3:8) angles(9:end)-90];
 angles = [angles angles2];
-angles = mod(angles-90,360);
+angles = mod(angles+90,360);
 eccArray = [eccArray eccArray(3:8) fliplr(eccArray(9:end))];
 
 if length(eccArray) ~= length(angles)
@@ -39,15 +39,13 @@ for curBlock = 0:15:345
         orig_ang = eccArray(angles==curBlock);
         final_ang = eccArray(angles==mod(curBlock+15,360));
         interpEcc = (final_ang-orig_ang) * withinB / 15 + orig_ang;
-        disp(sprintf('%i: %.03f',curBlock,interpEcc));
+%         disp(sprintf('%i: %.03f',curBlock,interpEcc));
         eccs = [eccs interpEcc];
     end
 end
 
 mglGluPartialDisk(xs,ys,zeros(size(xs)),eccs,sAs,ones(size(xs))*2,ones(3,size(xs,2)));
 mglGluPartialDisk(xs,ys,zeros(size(xs)),eccs,sAs,-ones(size(xs))*2,ones(3,size(xs,2)));
-
-mglFlush
 
 mglStencilCreateEnd;
 mglClearScreen();
