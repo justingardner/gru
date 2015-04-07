d66d707d8567f6f07d11a9e434a3412532caea0e
dbirman <danbirman@gmail.com>
2015-04-07 14:20:12 -0700

    Updated mlrGetSurf
    No longer relies on a saved local file, checks the LXC server directly
    for segmentations.
    Copies segmentations locally, then pushes to AnatDB.
    Still requires password directly—todo

--- mlrGetSurf.m
+++ mlrGetSurf.m
@@ -1,4 +1,4 @@
-function success = mlrGetSurf(project,password)
+function mlrGetSurf()
 %   mlrGetSurf(project)
 %
 % GOAL:
@@ -17,120 +17,231 @@ function success = mlrGetSurf(project,password)
 %   success     1 when your files were successfully moved onto this local
 %               computer. 0 otherwise.
 
-%% Build up our collection of files
-possibleFiles = dir(fullfile('~/data/',project,'rP*.mat'));
-doFiles = {};
-for pi = 1:length(possibleFiles)
-    cFile = possibleFiles(pi).name;
-    if strcmp(input(sprintf('Get Surfaces for file: %s? y/n... ',cFile),'s'),'y')
-        doFiles{end+1} = cFile;
-    end
+%% Search the LXC server for subjects
+s.localDataDir= '~/data/mlrAnatDB';
+s.cniComputerName = 'cnic7.stanford.edu';
+
+s = getInfo(s);
+
+command = sprintf('ssh %s@%s ls /data/freesurfer/subjects/',s.sunetID,s.cniComputerName);
+disp(command);
+disp('Enter password: ');
+[status,result] = system(command);
+
+%% Parse result for subject folders
+allSubjPos = regexp(result,'s\d\d\d\d');
+subjFolders = {};
+for i = 1:length(allSubjPos)
+    subjFolders{end+1} = result(allSubjPos(i):allSubjPos(i)+4);
 end
 
-for fi = 1:length(doFiles)
-    cFile = doFiles{fi};
-    cFile = fullfile('~/data/',project,cFile);
-    if mlrGetSurfHelper(cFile,password);
-        delete(cFile);
-    else
-        disp(sprintf('Surfaces for %s failed to copy, you should have already gotten a more verbose explanation',cFile));
-    end
-
+%% Check what folder we want to move
+disp('Which folder(s) do you want to copy locally: e.g. 1, or [1 2]');
+for i = 1:length(subjFolders);
+    disp(sprintf('%i: %s',i,subjFolders{i}));
 end
+
+values = [];
+while isempty(values)
+    values = input('Folders: ');
+    if ~isnumeric(values), values = []; end
+end
+
+%% Run mlrGetSurfHelper
+
+for i = 1:length(values)
+    mlrGetSurfHelper(subjFolders{values(i)},s);
+end
+
 %% Helpers
 
-function success = mlrGetSurfHelper(file,password)
-    
-%% Load our file
-load(file);
+function success = mlrGetSurfHelper(folder,s)
+%% Setup
+
+s.aDBServ = fullfile('https://gru.stanford.edu/hg/mlrAnatDB',folder);
+s.aDBLocal = fullfile('~/data/mlrAnatDB',folder);
+s.subjectID = folder;
+
+%% 
+s.fstempPath = fullfile('/data/freesurfer/subjects/',folder);
 
 %% Check if we already ran this...
-if checkForSurfFiles(reconParams.localDataPath)
+if checkForSurfFiles(s.localDataDir)
     warning('It looks like you already ran mlrReconAll for this subject, this function will overwrite the files.');
+    keyboard
 end
 
 %% Check if we are at Stanford in GRU lab (171.64.40.***)
-ipPlus =  urlread('http://checkip.dyndns.org/');
-if isempty(strfind(ipPlus,'171.64.40'))
-    error('mlrReconAll and mlrGetSurf are only intended for use at Stanford, with the NIMS database!');
+% ipPlus =  urlread('http://checkip.dyndns.org/');
+% if isempty(strfind(ipPlus,'171.64.40'))
+%     error('mlrReconAll and mlrGetSurf are only intended for use at Stanford!');
+% end
+
+%% Folder Structure
+if ~isdir(s.aDBLocal)
+    disp(sprintf('Please copy into a terminal, then dbcont:'));
+    disp(sprintf('hg clone %s %s',s.aDBServ,s.aDBLocal));
+    keyboard
+else
+    disp('Anatomy Database exists for subject. ');
+    cur = pwd;
+    cd(s.aDBLocal);
+    system('hg pull');
+    cd(cur);
+end
+base = {'3D','mlrBaseAnatomies','niftiROIS','localizers','mlrROIs'};
+    
+succ = 1;
+
+if ~isdir(s.aDBLocal)            
+    succ = 0;
+else
+    succ = 1;
+    for i = 1:length(base)
+        if ~isdir(fullfile(s.aDBLocal,base{i}))
+            succ = 0;
+        end
+    end
+end
+if ~succ
+    error(sprintf('Anatomy Database folder structure for %s is not correct.',s.subjectID));
 end
 
-%% Check that reconParams has everything necessary
-reconParams.conn = ssh2_config(reconParams.LXCServer,reconParams.user,password);
-
-if ~isfield(reconParams,'conn') || ~isfield(reconParams,'LXCServer') || ~isfield(reconParams,'fstempPath') || ~isfield(reconParams,'localDataPath') || ~isfield(reconParams,'user')
-    error('reconParams was not built correctly... cannot continue');
-end
-
-%% Connect to the LXC Server
+%% Connect to LXC Server
 try
     curDir = pwd;
     cd('~/proj/gru');
     addpath(genpath('~/proj/gru/ssh2_v2_m1_r6'));
-    ssh2_conn = ssh2_config(reconParams.LXCServer,reconParams.user,password);
+    s.conn = ssh2_config(s.cniComputerName,s.sunetID,input('Password: ','s'));
     cd(curDir);
 catch e
     warning('Something went wrong with ssh2, are you sure you have the ssh2 folder on your path?');
     error(e);
 end
+s.conn = s.conn;
 
 %% Check if FreeSurfer is finished
-reconParams.fstempPath = '/data/freesurfer/subjects/fs_dan';
 
-ssh2_conn = ssh2_command(ssh2_conn,sprintf('ls %s',fullfile(reconParams.fstempPath,'surf')));
+s.fstempPath = fullfile('/data/freesurfer/subjects/',folder);
+s.fstempPathMRI = fullfile(s.fstempPath,'mri');
+s.fstempPathSurf = fullfile(s.fstempPath,'surf');
 
-if isempty(ssh2_conn.command_result{1})
-    warning('FreeSurfer didn''t finish running. Be patient!');
-    success = 0;
-    return
+% Check if the T1 is finished
+command = sprintf('ls %s',s.fstempPathMRI);
+s.conn = ssh2_command(s.conn,command);
+
+pos = findFile(s.conn,'T1.mgz');
+
+exist_t1 = 1;
+if ~pos
+    exist_t1 = 0;
+else
+    disp('Found T1.mgz file');
+    while ~findFile(s.conn,'T1.nii')
+        disp('Converting T1.mgz to T1.nii');
+        command = sprintf('ssh %s@%s\nmri_convert %s %s',s.sunetID,s.cniComputerName,fullfile(s.fstempPathMRI,'T1.mgz'),fullfile(s.fstempPathMRI,'T1.nii'));
+        disp('Enter the following commands into any terminal [dbcont when complete]:');
+        disp(command);
+        keyboard
+        command = sprintf('ls %s',s.fstempPathMRI);
+        s.conn = ssh2_command(s.conn,command);
+    end
 end
-disp('FreeSurfer appears to have completed, checking for specific files.');
+if exist_t1
+    disp('T1.nii file exists, will copy as canonical.');
+end
 
-warning('Specific file checking is not implemented yet... if freesurfer didn''t completely finish you will be missing files');
+%% Check for Surfaces
+command = sprintf('ls %s',s.fstempPathSurf);
+s.conn = ssh2_command(s.conn,command);
+
+
+files = {{'lh.pial' 'rh.pial' 'lh.smoothwm' 'rh.smoothwm' 'lh.inflated' 'rh.inflated'}, {'T1.mgz', 'T1.nii'}};
+
+exist_surf = 1;
+if ~findFile(s.conn,'lh.pial') || ~findFile(s.conn,'rh.pial') || ~findFile(s.conn,'lh.smoothwm') || ~findFile(s.conn,'rh.smoothwm')
+    warning('FreeSurfer didn''t finish running. Re-run this later to copy the surfaces.');
+    exist_surf = 0;
+end
+
+if exist_surf
+    disp('FreeSurfer appears to have completed, will copy surfaces.');
+end
 
 %% SCP Files to /data/
-localFullPath = reconParams.localDataPath;
-scpCommand = sprintf('scp -r %s@%s:%s %s',reconParams.user,reconParams.LXCServer,reconParams.fstempPath,localFullPath);
+i = 1;
+if ~isdir(fullfile(s.aDBLocal,'mlrBaseAnatomies','FS'))
+    mkdir(fullfile(s.aDBLocal,'mlrBaseAnatomies','FS'));
+end
+s.fullLocal = fullfile(s.aDBLocal,'mlrBaseAnatomies','FS',num2str(i));
+while isdir(s.fullLocal)
+    warning('Found existing directory %s.',s.fullLocal);
+    in = input('Do you want to overwrite? [y/n]: ','s');
+    if strcat(in,'y')
+        break
+    end
+    i = i+1;
+    s.fullLocal = fullfile(s.aDBLocal,'mlrBaseAnatomies','FS',num2str(i));
+end
 
-disp(sprintf('\nPlease copy and paste the following into a terminal:\n\n%s\n\nWhen the commands finish, type ''dbcont'' to keep going',scpCommand));
+scpCommand = sprintf('scp -r %s@%s:%s %s',s.sunetID,s.cniComputerName,s.fstempPath,s.fullLocal);
 
-keyboard
+disp('Copying files locally.');
+disp(scpCommand);
+system(scpCommand);
 
 %% Check that SCP succeeded
-success = checkForSurfFiles(reconParams.localDataPath);
-if ~success
-    disp(sprintf('Use these commands to check whether FreeSurfer spit out all the correct files:\n\nssh -XY %s@%s\ncd %s/surf\nls\n\nYou should see these files (among others): lh.pial, lh.inflated, and lh.smoothwm',reconParams.user,reconParams.LXCServer,reconParams.fstempPath));
-    error('File copy failed... Are you sure FreeSurfer finished successfully?');
+[s_t1 s_surf] = checkForSurfFiles(s.fullLocal);
+if ~s_surf
+    warning('No surface files.');
+end
+if ~s_t1
+    warning('No canonical files.');
 end
 
-%% Check if mrInit was run
-warning('Not sure how to check for mrInit yet...');
-mrInitFlag = 1;
 %% Run mlrImportFreeSurfer
-if mrInitFlag
+if s_surf
     cDir = pwd;
-    cd (reconParams.localDataPath)
+    cd (s.fullLocal)
     mlrImportFreeSurfer
     cd(cDir);
-else
-    warning('mlrImportFreeSurfer was not run, did you run mrInit?');
+    warning('mlrImportFreeSurfer was not run.');
 end
 
-%% Cleanup the server, get rid of the /subj and /fs_subj directories
-if success
+%% Copy canonical T1
+if s_t1
+    file = fullfile(s.fullLocal,'mri','T1.nii');
+    nfile = fullfile(s.aDBLocal,'3D','T1w_Canonical.nii');
+    copyfile(file,nfile);
+end
+
+%% Push to hg server
+cur = pwd;
+cd(s.aDBLocal);
+system(sprintf('hg commit -m "Added segmentation files for %s"',s.subjectID));
+system(sprintf('hg push'));
+cd(cur);
+
+%% Cleanup the server, get rid of the /data/temp/s#### and /data/freesurfer/subjects/s####
+if s_t1 && s_surf
     if strcmp(input('Do you want to remove the folders on the LXC server? y/n: ','s'),'y')
-        ssh2_conn = ssh2_command(ssh2_conn,sprintf('rm -rf %s',reconParams.tempPath));
-        ssh2_conn = ssh2_command(ssh2_conn,sprintf('rm -rf %s',reconParams.fstempPath));
+        s.conn = ssh2_command(s.conn,sprintf('rm -rf %s',fullfile('/data/temp/',s.subjectID)));
+        s.conn = ssh2_command(s.conn,sprintf('rm -rf %s',fullfile(s.fstempPath)));
         disp('Files removed...');
     end
 end
 
 %% Close lxc
-ssh2_close(ssh2_conn);
+ssh2_close(s.conn);
+s.conn = struct;
 
-function success = checkForSurfFiles(dir)
+%% 
+disp('mlrGetSurf for subject %s is complete.',s.subjectID);
+
+function [s_t1, s_surf] = checkForSurfFiles(dir)
 dirs = {'surf','mri'};
-files = {{'lh.pial' 'rh.pial' 'lh.smoothwm' 'rh.smoothwm' 'lh.inflated' 'rh.inflated'}, {'T1.mgz'}};
+files = {{'lh.pial' 'rh.pial' 'lh.smoothwm' 'rh.smoothwm' 'lh.inflated' 'rh.inflated'}, {'T1.mgz', 'T1.nii'}};
+success = [1 1];
 for di = 1:length(dirs)
     cdir = dirs{di};
     cfiles = files{di};
@@ -138,10 +249,95 @@ for di = 1:length(dirs)
         cfile = cfiles{fi};
         % check for file
         if ~isfile(fullfile(dir,cdir,cfile))
-            success = 0;
-            return
+            success(di) = 0;
         end
     end
 end
-success = 1;
+s_t1 = success(2);
+s_surf = success(1);
 return
+
+
+
+function s = getInfo(s)
+
+s.cniDir = [];
+
+s.sunetID = mglGetParam('sunetID');
+if isempty(s.sunetID),s.sunetID = getusername;,end
+
+% put up dialog making sure info is correct
+mrParams = {{'cniComputerName',s.cniComputerName,'The name of the computer to ssh into'},...
+	    {'sunetID',s.sunetID,'Your sunet ID'}};
+params = mrParamsDialog(mrParams,'Login information');
+if isempty(params),return,end
+
+% save sunetID
+if ~isempty(params.sunetID) mglSetParam('sunetID',params.sunetID,1);end
+
+% get some variables into system variable
+s.sunetID = params.sunetID;
+s.cniComputerName = params.cniComputerName;
+
+
+%%%%%%%%%%%%%%%%%%%%%
+%%   getusername   %%
+%%%%%%%%%%%%%%%%%%%%%
+% getusername.m
+%
+%      usage: getusername.m()
+%         by: justin gardner
+%       date: 09/07/05
+%
+function username = getusername()
+
+[retval username] = system('whoami');
+% sometimes there is more than one line (errors from csh startup)
+% so need to strip those off
+username = strread(username,'%s','delimiter','\n');
+username = username{end};
+if (retval == 0)
+  % get it again
+  [retval username2] = system('whoami');
+  username2 = strread(username2,'%s','delimiter','\n');
+  username2 = username2{end};
+  if (retval == 0)
+    % find the matching last characers
+    % this is necessary, because matlab's system command
+    % picks up stray key strokes being written into
+    % the terminal but puts those at the beginning of
+    % what is returned by stysem. so we run the
+    % command twice and find the matching end part of
+    % the string to get the username
+    minlen = min(length(username),length(username2));
+    for k = 0:minlen
+      if (k < minlen)
+	if username(length(username)-k) ~= username2(length(username2)-k)
+	  break
+	end
+      end
+    end
+    if (k > 0)
+      username = username(length(username)-k+1:length(username));
+      username = lower(username);
+      username = username(find((username <= 'z') & (username >= 'a')));
+    else
+      username = 'unknown';
+    end
+  else
+    username = 'unknown';
+  end
+else
+  username = 'unknown';
+end
+
+function pos = findFile(conn,file)
+findFile = cellfun(@strfind,conn.command_result,repmat({file},size(conn.command_result)),'UniformOutput',false);
+if isempty(findFile)
+    pos = 0;
+else
+    pos = find(logical(1-cellfun(@isempty,findFile))==1);
+    if isempty(pos)
+        pos = 0;
+    end
+end
