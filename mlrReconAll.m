da6d2bfd2214b81d746c7b692881ed7e3e5182e8
dbirman <danbirman@gmail.com>
2015-04-07 14:15:56 -0700

    Removed ip check and added helper

--- mlrReconAll.m
+++ mlrReconAll.m
@@ -36,11 +36,11 @@ if isempty(s.cniDir),return,end
 
 
 %% Check if we are at Stanford in GRU lab (171.64.40.***)
-disp('Checking ip address...');
-ipPlus =  urlread('http://checkip.dyndns.org/');
-if isempty(strfind(ipPlus,'171.64.40'))
-    error('mlrReconAll and mlrGetSurf are only intended for use at Stanford, with the NIMS database!');
-end
+% disp('Checking ip address...');
+% ipPlus =  urlread('http://checkip.dyndns.org/');
+% if isempty(strfind(ipPlus,'171.64.40'))
+%     error('mlrReconAll and mlrGetSurf are only intended for use at Stanford, with the NIMS database!');
+% end
 
 %% Fill out s
 
@@ -225,3 +225,54 @@ end
 % set cniDir in system variable
 s.cniDir = fullfile(dirName,scanName);
 disp(sprintf('(dofmricni:getCNIDir) Directory chosen is: %s',s.cniDir))
+
+%%%%%%%%%%%%%%%%%%%%%
+%%   getusername   %%
+%%%%%%%%%%%%%%%%%%%%%
+% getusername.m
+%
+%      usage: getusername.m()
+%         by: justin gardner
+%       date: 09/07/05
+%
+function username = getusername()
+
+[retval username] = system('whoami');
+% sometimes there is more than one line (errors from csh startup)
+% so need to strip those off
+username = strread(username,'%s','delimiter','\n');
+username = username{end};
+if (retval == 0)
+  % get it again
+  [retval username2] = system('whoami');
+  username2 = strread(username2,'%s','delimiter','\n');
+  username2 = username2{end};
+  if (retval == 0)
+    % find the matching last characers
+    % this is necessary, because matlab's system command
+    % picks up stray key strokes being written into
+    % the terminal but puts those at the beginning of
+    % what is returned by stysem. so we run the
+    % command twice and find the matching end part of
+    % the string to get the username
+    minlen = min(length(username),length(username2));
+    for k = 0:minlen
+      if (k < minlen)
+	if username(length(username)-k) ~= username2(length(username2)-k)
+	  break
+	end
+      end
+    end
+    if (k > 0)
+      username = username(length(username)-k+1:length(username));
+      username = lower(username);
+      username = username(find((username <= 'z') & (username >= 'a')));
+    else
+      username = 'unknown';
+    end
+  else
+    username = 'unknown';
+  end
+else
+  username = 'unknown';
+end
