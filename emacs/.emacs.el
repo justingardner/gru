8ce0a3c6fc67f28d2675a953e72d1844269c4ad0
Justin Gardner <jlg@stanford.edu>
2015-08-26 08:49:52 -0700

    Emacs startup point to gru folder

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -343,7 +343,7 @@ the leading comment-start from each line, if any."
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  )
-(load "~/proj/matlab/matlab.el")
+(load "~/proj/gru/emacs/matlab.el")
 (setq default-frame-alist '(
                              (border-color          . "green")
                              (foreground-color      . "white")
