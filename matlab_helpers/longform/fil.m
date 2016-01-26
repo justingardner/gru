3e31e367a12cfb64a2f207044ba52d40023fcdd9
dbirman <danbirman@gmail.com>
2016-01-26 12:15:10 -0800

    long filtering

---
+++ matlab_helpers/longform/fil.m
@@ -0,0 +1,15 @@
+
+function data = fil(data,col,comparison,value)
+% LONGFORM FILTER(data,column,==value)
+
+scol = data(:,col);
+switch comparison
+    case '>'
+        data = data(scol>value,:);
+    case '<'
+        data = data(scol<value,:);
+    case '>='
+        data = data(scol>=value,:);
+    case '<='
+        data = data(scol<=value,:);
+end
