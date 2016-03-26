1de764af8b779a4905039bb944c18f0ec65c0b11
gru_cni <gru in cni>
2016-03-25 21:00:01 -0700

    update

--- GRUExpProtocol/RetStanfordBarsTaskVsBarsFix/RetStanfordBarsTaskVsBarsFix.m
+++ GRUExpProtocol/RetStanfordBarsTaskVsBarsFix/RetStanfordBarsTaskVsBarsFix.m
@@ -36,52 +36,52 @@
 
 %mux 8 arc 1: 8 cycle * 48 vols/cycle + 3(+1 initial) blanks * 24vols = 480 vols + 8(mux)*2(nummux) = 493 vols (4:06min)
 %% Scan 1 (BarsTask)
-mglRetinotopy('displayName=fMRIproj32','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% Scan 2 (BarsTaskFixation)
-mglRetinotopy('displayName=fMRIproj32','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% Scan 3 (BarsTask)
-mglRetinotopy('displayName=fMRIproj32','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% Scan 4 (BarsTaskFixation)
-mglRetinotopy('displayName=fMRIproj32','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% Scan 5 (BarsTask)
-mglRetinotopy('displayName=fMRIproj32','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% Scan 6 (BarsTaskFixation)
-mglRetinotopy('displayName=fMRIproj32','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% Scan 7 (BarsTask)
-mglRetinotopy('displayName=fMRIproj32','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% Scan 8 (BarsTaskFixation)
-mglRetinotopy('displayName=fMRIproj32','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% Scan 9 (BarsTask)
-mglRetinotopy('displayName=fMRIproj32','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% Scan 10 (BarsTaskFixation)
-mglRetinotopy('displayName=fMRIproj32','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% Scan 11 (BarsTask)
-mglRetinotopy('displayName=fMRIproj32','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% Scan 12 (BarsTaskFixation)
-mglRetinotopy('displayName=fMRIproj32','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% Scan 13 (BarsTask)
-mglRetinotopy('displayName=fMRIproj32','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% Scan 14 (BarsTaskFixation)
-mglRetinotopy('displayName=fMRIproj32','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% Scan 15 (BarsTask)
-mglRetinotopy('displayName=fMRIproj32','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsTask=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% Scan 16 (BarsTaskFixation)
-mglRetinotopy('displayName=fMRIproj32','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
+mglRetinotopy('displayName=fMRIprojFlex','barsFix=1','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',48,'blanks=3','doEyeCalib=0');
 
 %% ...
 
