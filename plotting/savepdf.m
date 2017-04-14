eb37bd7701cf0c977368e52bd03dc6e551ce8630
Dan Birman <danbirman@gmail.com>
2017-04-13 17:47:27 -0700

    Updates to drawPublishAxis and savepdf
     - Added legend transparency to savepdf
     - Fixed subplot issues
     - Added a flag in drawPublishAxis to check who called it

--- plotting/savepdf.m
+++ plotting/savepdf.m
@@ -3,8 +3,14 @@ function savepdf(h,fname,varargin)
 figure(h);
 
 % Use nature figure sizes: 247, 183, or 89 mm
-figsize = 0;
-getArgs(varargin,{'figsize=1'});
+
+% For 4-figure 247 use 5.8 width and 10 height
+% For 3-figure 247 use 8 width and 10 height
+
+
+%% Change the figure size
+figsize = 0; subplots = 0;
+getArgs(varargin,{'figsize=0','subplots=0'});
 
 set(h,'Units','Centimeters');
 pos = get(h,'Position');
@@ -13,6 +19,12 @@ set(gcf,'Color',[1 1 1]);
 set(gca,'Color',[1 1 1]);
 
 set(h,'PaperPositionMode','Auto','PaperUnits','Centimeters');
+
+if length(figsize)==2
+    pos(4) = figsize(2);
+    pos(3) = figsize(1);
+    figsize = figsize(1);
+end
 
 switch figsize
     case 0
@@ -32,7 +44,22 @@ end
 set(h,'Position',[0, 0, figsize, pos(4)*figsize/pos(3)]);
 
 % DPA
-drawPublishAxis
+if ~subplots
+    % Remove the legend box + background
+    l = legend(gca,'boxoff');
+    set(l,'Color','none');
+    drawPublishAxis('calledbysavepdf=1');
+else
+    for x = 1:subplots(1)
+        for y = 1:subplots(2)
+            subplot(subplots(1),subplots(2),(x-1)*subplots(2)+y);
+            % Remove the legend box + background
+            l = legend(gca,'boxoff');
+            set(l,'Color','none');
+            drawPublishAxis('calledbysavepdf=1');
+        end
+    end
+end
 
 pos = get(h,'Position');
 set(h,'Papersize',[pos(3) pos(4)]);
