d586f552586818b39d8958cdd4f21bf279e9aca2
Dan Birman <danbirman@gmail.com>
2018-12-06 15:33:29 -0800

    Removing old proejcts from local startup

--- startupdan.m
+++ startupdan.m
@@ -1,5 +1,5 @@
 
-pathNames = {'~/proj/att_awe','~/proj/unlearning','~/Box Sync/COHCON_DATA'};
+pathNames = {'~/proj/att_awe','~/proj/afcom'};
 
 addpath('~/proj/gru/mac_helpers'); % explicitly do this so we can use genpath_exclude to remove .git/.svn
 for i = 1:length(pathNames)
