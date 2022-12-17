0140fb625ae83233890ce2ba8d8c604e6879cd93
Justin Gardner <jlg@stanford.edu>
2022-12-16 17:11:15 -0800

    adding emri analysis

---
+++ analysis/emri/emriAnal.m
@@ -0,0 +1,234 @@
+function [v params] = emriAnal(v,params,varargin)
+%
+%      usage: emriAnal(v,params,varargin)
+%         by: justin gardner (Adapted from corAnal and pRF code)
+%       date: 12/16/2022
+%    purpose: compute various analyses for DIANA sequence data
+%
+%             if you just want a default parameter structure you
+%             can do:
+% 
+%             v = newView;
+%             [v params] = emriAnal(v,[],'justGetParams=1','defaultParams=1','scanList=1')
+%
+
+% check arguments
+if nargin < 1
+  help emri
+  return
+end
+
+d = [];
+
+% params defaults to empty
+if nargin < 2,params =[];end
+
+% other arguments
+justGetParams=[];defaultParams=[];scanList=[];groupNum=[];
+getArgs(varargin,{'justGetParams=0','defaultParams=0','scanList=[]','groupNum=[]'});
+
+% first get parameters
+if isempty(params)
+  % get group
+  if isempty(groupNum),groupNum = viewGet(v,'curGroup');end
+  % put up the gui
+  params = emriGUI('v',v,'groupNum',groupNum,'defaultParams',defaultParams,'scanList',scanList);
+end
+
+% just return parameters
+if justGetParams,d = params;return,end
+
+% Reconcile params with current status of group and ensure that it has
+% the required fields. 
+params = defaultReconcileParams([],params);
+
+% Abort if params empty
+if isempty(params),return,end
+
+% check the params
+params = checkEmriParams(params);
+
+% set the group
+v = viewSet(v,'curGroup',params.groupName);
+
+if params.frequencyAnalysis
+  runCorAnal(v,params);
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% initOverlay
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function o = initOverlay(v,nScans,varargin)
+
+getArgs(varargin,{'name=default','functionName=emriAnal','reconcileFunction=defaultReconcileParams','mergeFunction=defaultMergeParams','colormap',[],'interrogatorFunction=emriPlot','params',[],'overlayRange',[0 1]});
+
+o.name = name;
+o.function = functionName;
+o.reconcileFunction = reconcileFunction;
+o.mergeFunction = mergeFunction;
+if isempty(colormap)
+  o.colormap = jet(256);
+else
+  o.colormap = colormap;
+end    
+o.date = datestr(now);
+o.interrogator = interrogatorFunction;
+o.groupName = viewGet(v,'groupName');
+o.params = params;
+o.range = overlayRange;
+co.data = cell(1,nScans);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% runCorAnal
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function runCorAnal(v,params)
+
+% get number of scans
+nScans = length(params.scanNum);
+
+% set up the params structure as if this were a corAnal
+corAnalParams.recompute = zeros(1,viewGet(v,'nScans'));
+corAnalParams.recompute(params.scanNum) = 1;
+for iScan = 1:viewGet(v,'nScans')
+  corAnalParams.ncycles(iScan) = nan;
+  corAnalParams.detrend{iScan} = params.detrend;
+  if params.divideByMean
+    corAnalParams.spatialnorm{iScan} = 'Divide by mean';
+  else
+    corAnalParams.spatialnorm{iScan} = 'None';
+  end
+  corAnalParams.trigonometricFunction{iScan} = params.trigonometricFunction;
+end
+
+co = {};amp={};ph={};
+% run correlation analysis at each frequency 
+for iFreq = params.cyclesScanMin:params.cyclesScanMax
+
+  % set the frequency for the analysis
+  corAnalParams.ncycles(:) = iFreq;
+  
+  % init overlays
+  co{end+1} = initOverlay(v,nScans,'name',sprintf('%03i co',iFreq),'params',corAnalParams,'overlayRange',[0 1]);
+  amp{end+1} = initOverlay(v,nScans,'name',sprintf('%03i amp',iFreq),'params',corAnalParams,'overlayRange',[0 1]);
+  ph{end+1} = initOverlay(v,nScans,'name',sprintf('%03i ph',iFreq),'params',corAnalParams,'overlayRange',[0 2*pi]);
+
+  % Compute it (function is a copy of what is in corAnal
+  [co{end},amp{end},ph{end}] = computeCorrelationAnalysis(v,corAnalParams,co{end},amp{end},ph{end});
+
+  % Fill range field for amp
+  ampMin = realmax; ampMax = 0;
+  for iScan=1:nScans
+    if ~isempty(amp{end}.data{iScan})
+      ampMin = min([ampMin min(amp{end}.data{iScan}(:))]);
+      ampMax = max([ampMax max(amp{end}.data{iScan}(:))]);
+    end
+  end
+  if (ampMin <= ampMax)
+    amp{end}.range = [ampMin ampMax];
+  end
+end
+
+% Install emriAnal in the view
+emriAnal.name = 'emriAnal';  % This can be reset by editAnalysisGUI
+emriAnal.type = 'emriAnal';
+emriAnal.groupName = params.groupName;
+emriAnal.function = 'emriAnal';
+emriAnal.reconcileFunction = 'defaultReconcileParams';
+emriAnal.mergeFunction = 'defaultMergeParams';
+emriAnal.guiFunction = 'emriGUI';
+emriAnal.overlayInterpFunction = 'corAnalInterp';
+emriAnal.params = params;
+emriAnal.date = datestr(now);
+v = viewSet(v,'newanalysis',emriAnal);
+for iOverlay = 1:length(co)
+  v = viewSet(v,'newoverlay',co{iOverlay});
+  v = viewSet(v,'newoverlay',amp{iOverlay});
+  v = viewSet(v,'newoverlay',ph{iOverlay});
+end
+if ~isempty(viewGet(v,'fignum'))
+  refreshMLRDisplay(viewGet(v,'viewNum'));
+end
+
+% Save it
+saveAnalysis(v,emriAnal.name);
+
+return;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [co,amp,ph] = computeCorrelationAnalysis(view,params,co,amp,ph)
+% Required fields in params: 'recompute','ncycles','detrend','spatialnorm'
+
+% Get scanList from params.recompute field
+scanList = find(params.recompute(:));
+
+disp('Computing corAnal...');
+warning('off','MATLAB:divideByZero');
+for scanIndex=1:length(scanList)
+  scanNum = scanList(scanIndex);
+  waitHandle = mrWaitBar(0,['Computing Correlation Analysis for scan ' int2str(scanNum) ':']);
+
+  % sliceDims: [ydim xdim] for single slice
+  % volDims; [ydim xdim nslices] for single scan
+  sliceDims = viewGet(view,'sliceDims',scanNum);
+  volDims = viewGet(view,'dims',scanNum);
+
+  % Initialize data with NaNs
+  co.data{scanNum} = NaN*ones(volDims);
+  amp.data{scanNum} = NaN*ones(volDims);
+  ph.data{scanNum} = NaN*ones(volDims);
+
+  % check for corAnal cycles set to 0
+  if params.ncycles(scanList(scanIndex)) == 0
+    mrWarnDlg(sprintf('(corAnal:computeCorrelationAnalysis) !!! Scan %i has ncycles set to 0 - this needs to be set to how many cycles of the stimulus you had per scan. Skipping this scan !!!',scanList(scanIndex)));
+    continue;
+  end
+  
+  nslices = viewGet(view,'nslices',scanNum);
+  for sliceNum = 1:nslices
+    % Analysis parameters for this scan
+    junkframes = viewGet(view,'junkframes',scanNum);
+    nframes = viewGet(view,'nframes',scanNum);
+    % Load tSeries
+    tSeries = loadTSeries(view, scanNum, sliceNum);
+    % Reshape the tSeries
+    % ATTN: added reshapeTSeries function, since loadTSeries not longer reshapes when it loads -eli
+    tSeries = reshapeTSeries(tSeries);
+    
+    % check that junkframes and nframes settings are ok
+    if size(tSeries,1) < (junkframes+nframes)
+      mrErrorDlg(sprintf('(corAnal) Number of junkframes (%i) plus nframes (%i) should not be larger than number of volumes in scan %i',junkframes,nframes,size(tSeries,1)));
+    end
+    % Remove junkFrames
+    tSeries = tSeries(junkframes+1:junkframes+nframes,:);
+    %compute corAnal
+    [coSeries,ampSeries,phSeries] = computeCoranal(tSeries,params.ncycles(scanNum),params.detrend{scanNum},params.spatialnorm{scanNum},params.trigonometricFunction{scanNum});
+
+    switch view.viewType
+      case {'Volume'}
+          co.data{scanNum}(:,:,sliceNum) = reshape(coSeries,sliceDims);
+          amp.data{scanNum}(:,:,sliceNum) = reshape(ampSeries,sliceDims);
+          ph.data{scanNum}(:,:,sliceNum) = reshape(phSeries,sliceDims);
+      case {'Surface'}
+          co.data{scanNum} = coSeries;
+          amp.data{scanNum} = ampSeries;
+          ph.data{scanNum} = phSeries;
+      case {'Flat'}
+          co.data{scanNum}(:,:,sliceNum) = reshape(coSeries,sliceDims);
+          amp.data{scanNum}(:,:,sliceNum) = reshape(ampSeries,sliceDims);
+          ph.data{scanNum}(:,:,sliceNum) = reshape(phSeries,sliceDims);
+    end
+    % Update waitbar
+    mrWaitBar(sliceNum/nslices,waitHandle);
+  end
+  mrCloseDlg(waitHandle);
+end
+warning('on','MATLAB:divideByZero');
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+%    checkEmriParams    %
+%%%%%%%%%%%%%%%%%%%%%%%%
+function params = checkEmriParams(params)
+
+% just a stub for now in case we have things we need to check
---
+++ analysis/emri/emriGUI.m
@@ -0,0 +1,98 @@
+% pRFGUI.m
+%
+%        $Id:$ 
+%      usage: emriGUI()
+%         by: justin gardner (based on pRFGUI)
+%       date: 12/16/2022
+%    purpose: GUI for getting params for emriAnal
+%
+function params = emriGUI(varargin)
+
+% get the arguments
+params=[];groupNum=[];defaultParams=[];scanList = [];v = [];
+getArgs(varargin,{'params=[]','groupNum=[]','defaultParams=0','scanList=[]','v=[]'});
+
+% if called with params, then just display
+if ~isempty(params)
+  retval = dispParams(params);
+  if isempty(retval),params = [];end
+  return
+end
+
+% get a view
+deleteViewOnExit = false;
+if isempty(v),v = newView;deleteViewOnExit = true;end
+
+% get the group names put on top passed in group if set
+groupNames = putOnTopOfList(viewGet(v,'groupName',groupNum),viewGet(v,'groupNames'));
+
+% set the parameter string
+paramsInfo = {};
+paramsInfo{end+1} = {'groupName',groupNames,'Name of group from which to do pRF analysis'};
+paramsInfo{end+1} = {'saveName','emriAnal','File name to try to save as'};
+
+% parameters
+paramsInfo{end+1} = {'frequencyAnalysis',1,'type=checkbox','Compute frequency analysis'};
+paramsInfo{end+1} = {'cyclesScanMin',1,'incdec',[-1 1],'minmax',[1 inf],'Compute frequency analysis starting at this frequency','contingent=frequencyAnalysis'};
+paramsInfo{end+1} = {'cyclesScanMax',5,'incdec',[-1 1],'minmax',[1 inf],'Compute frequency analysis ending at this frequency','contingent=frequencyAnalysis'};
+paramsInfo{end+1} = {'detrend',{'None','Highpass','Linear','Quadratic'},'Detrend for corAnal','contingent=frequencyAnalysis'};
+paramsInfo{end+1} = {'divideByMean',0,'type=checkbox','Divide by mean of time-series (not necessary for concatenation as this has already been done','contingent=frequencyAnalysis'};
+paramsInfo{end+1} = {'trigonometricFunction',{'Sine','Cosine'},'Sets which function phase will be used for frequency analysis','contingent=frequencyAnalysis'};
+
+% Get parameter values
+if defaultParams
+  params = mrParamsDefault(paramsInfo);
+else
+  params = mrParamsDialog(paramsInfo,'Set emriAnal parameters');
+end
+
+% if empty user hit cancel
+if isempty(params)
+  if deleteViewOnExit,deleteView(v);end
+  return
+end
+
+% get scans
+v = viewSet(v,'groupName',params.groupName);
+if ~isempty(scanList)
+  params.scanNum = scanList;
+elseif defaultParams
+  params.scanNum = 1:viewGet(v,'nScans');
+else
+  params.scanNum = selectScans(v);
+end
+if isempty(params.scanNum)
+  params = [];
+  if deleteViewOnExit,deleteView(v);end
+  return
+end
+
+if deleteViewOnExit,deleteView(v);end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% just display parameters
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function retval = dispParams(params)
+
+paramsInfo = {};
+% grab the parameters that are indicated in paramsInfo
+if isfield(params,'paramInfo')
+  % get the paramsInfo
+  topParamsInfo = params.paramInfo;
+  % go through each one
+  for i = 1:length(topParamsInfo)
+    % if it exists in the params filed then add it
+    if isfield(params,topParamsInfo{i}{1})
+      % and it to paramInfo
+      paramsInfo{end+1} = params.paramInfo{i};
+      % add the value from params 
+      paramsInfo{end}{2} = params.(topParamsInfo{i}{1});
+      % make it non editable
+      paramsInfo{end}{end+1} = 'editable=0';
+    end
+  end
+end
+
+retval = mrParamsDialog(paramsInfo,'pRF parameters');
+retval = [];
+
