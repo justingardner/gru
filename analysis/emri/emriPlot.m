9c52d35338c40495ade95166018fcc4c916d97b1
Justin Gardner <jlg@stanford.edu>
2022-12-19 12:49:01 -0800

    Cleaning up code to make it easier to add new analyses

--- analysis/emri/emriPlot.m
+++ analysis/emri/emriPlot.m
@@ -7,11 +7,52 @@
 %
 function emriPlot(v,overlayNum,scan,x,y,z,roi)
 
+% see if the shift key is down on MLR fig
+shiftDown = any(strcmp(get(viewGet(v,'figureNumber'),'SelectionType'),'extend'));
+
+% get time series
+[t, tSeries,tSeriesSte,headerStr] = getTSeries(v,scan,x,y,z,roi,shiftDown);
+if isempty(tSeries),return,end
+
+% calculate fft
+ft = fft(tSeries-mean(tSeries));
+absft = abs(ft(1:round((length(ft)/2))));
+
+% draw the basic time series
+nRows = 3; nCols = 3;
+emriPlotTSeries(t, tSeries, tSeriesSte, absft, nRows, nCols, headerStr);
+
+% get the coherence/amp/ph overlays with the largest coherence value
+[co,amp,ph] = getOverlaysWithMaxCoVal(v,scan,x,y,z,roi);
+if isempty(co),return,end
+
+% get parameters for corAnal and run it
+nCycles = co.params.ncycles(viewGet(v,'curScan'));
+detrend = viewGet(v,'detrend',scan);
+spatialnorm = viewGet(v,'spatialnorm',scan);
+trigonometricFunction = viewGet(v,'trigonometricFunction',scan);
+[coVal, ampVal, phVal, corAnalTSeries] = computeCoranal(tSeries,nCycles,detrend,spatialnorm,trigonometricFunction);
+
+% plot the cor anal
+emriPlotCorAnal(t, tSeries, tSeriesSte, absft, nCycles, coVal, ampVal, phVal, trigonometricFunction, nRows, nCols, headerStr)
+
+% plot the cross correlation
+%keyboard
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% getPeakCorrelationValue
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [co,amp,ph] = getOverlaysWithMaxCoVal(v,scan,x,y,z,roi)
+
+% default return values
+co = [];amp = [];ph = [];
+
+% get overlay names
 overlayNames = viewGet(v,'overlayNames');
 
 % look for the co overlay that has the highest coherence value for this
 % voxel
-maxCo = -inf;
+maxCo = -inf;maxCoOverlayNum = nan;
 for iOverlay = 1:length(overlayNames)
   if ~isempty(strfind(overlayNames{iOverlay},'co'))
     co = viewGet(v,'overlay',overlayNames{iOverlay});
@@ -19,7 +60,7 @@ for iOverlay = 1:length(overlayNames)
   % if the coherence value is greater than current maximum
   if co.data{scan}(x,y,z) > maxCo
     %then remember this and move to the next one
-    coOverlayNum = iOverlay;
+    maxCoOverlayNum = iOverlay;
     maxCo = co.data{scan}(x,y,z);
   end
 end
@@ -30,39 +71,36 @@ if maxCo == -inf
   return
 end
 
-% If corAnal is loaded, then use it. Otherwise, error.
-co = viewGet(v,'overlay',overlayNames{coOverlayNum});
-amp = viewGet(v,'overlay',overlayNames{coOverlayNum+1});
-ph = viewGet(v,'overlay',overlayNames{coOverlayNum+2});
+% load the coherence / amplitude and phase overlay that had the largest
+% coherence value
+co = viewGet(v,'overlay',overlayNames{maxCoOverlayNum});
+amp = viewGet(v,'overlay',overlayNames{maxCoOverlayNum+1});
+ph = viewGet(v,'overlay',overlayNames{maxCoOverlayNum+2});
 if isempty(co) || isempty(amp) || isempty(ph)
   disp(sprintf('(emriPlot) Could not find correct overlays'));
-  return
 end
 
-% see if the shift key is down on MLR fig
-shiftDown = any(strcmp(get(viewGet(v,'figureNumber'),'SelectionType'),'extend'));
+%%%%%%%%%%%%%%%%
+%% getTSeries
+%%%%%%%%%%%%%%%%
+function [t, tSeries,tSeriesSte,headerStr] = getTSeries(v,scan,x,y,z,roi,shiftDown)
 
-% get the peak frequency (i.e. the one with the highest frequency
-ncycles = co.params.ncycles(viewGet(v,'curScan'));;
+% default return parameters
+t = [];
+tSeries = [];
+tSeriesSte = [];
+headerStr = '';
 
-% Analysis parameters
-detrend = viewGet(v,'detrend',scan);
-spatialnorm = viewGet(v,'spatialnorm',scan);
+% frame clipping parameters
 junkframes = viewGet(v,'junkframes',scan);
 nframes = viewGet(v,'nframes',scan);
 framePeriod = viewGet(v,'framePeriod',scan);
-trigonometricFunction = viewGet(v,'trigonometricFunction',scan);
-
-if ncycles == 0
-  mrWarnDlg(sprintf('(corAnalPlot) Number of cycles per scan for the corAnal should not be: %i',ncycles));
-  return
-end
 
 % don't do roi if shift key is down
 if shiftDown
   roi = [];
 elseif length(roi) > 0
-  oneTimeWarning('eventRelatedPlotShiftKey',sprintf('(corAnalPlot) To avoid showing ROI plots, hold shift down when clicking'),1);
+  oneTimeWarning('eventRelatedPlotShiftKey',sprintf('(emriPlot) To avoid showing ROI plots, hold shift down when clicking'),1);
 end
 
 % now if we have an roi then load its time series
@@ -70,117 +108,166 @@ end
 % time series for the voxel
 roiPlot = 0;
 if ~isempty(roi)
-    roi = loadROITSeries(v,roi{1},viewGet(v,'curScan'),viewGet(v,'curGroup'));
-    tseries = mean(roi.tSeries,1)';
-    ptseriesSte = std(100*roi.tSeries/mean(tseries),1,1)'/sqrt(roi.n);
-    ptseriesSte = ptseriesSte(junkframes+1:junkframes+nframes);
-    headerStr = sprintf('Times series from roi %s (n=%i)',roi.name,roi.n);
-    roiPlot = 1;
+  roi = loadROITSeries(v,roi{1},viewGet(v,'curScan'),viewGet(v,'curGroup'));
+  tSeries = mean(roi.tSeries,1)';
+  tSeriesSte = std(100*roi.tSeries/mean(tSeries),1,1)'/sqrt(roi.n);
+  headerStr = sprintf('Times series from roi %s (n=%i)\n%s',roi.name,roi.n,viewGet(v,'description',viewGet(v,'curScan')));
+  roiPlot = 1;
 else
-    % Load tseries from file. Error if file doesn't exist.
-    pathStr = viewGet(v,'tseriesPathStr',scan);
-    if ~exist(pathStr,'file')
-        mrErrorDlg(['File ',pathStr,' not found']);
-    end
-    [tseries,hdr] = mlrImageReadNifti(pathStr,{x,y,z,[]});
-    headerStr = sprintf('Times series from voxel [%i %i %i] ',x,y,z);
-    tseries = squeeze(tseries);
+  % Load tseries from file. Error if file doesn't exist.
+  pathStr = viewGet(v,'tseriesPathStr',scan);
+  if ~exist(pathStr,'file')
+    disp(sprintf('(emriPlot) Could not find tSeries data file: %s',pathStr));
+    return
+  end
+  [tSeries,hdr] = mlrImageReadNifti(pathStr,{x,y,z,[]});
+  headerStr = sprintf('Times series from voxel [%i %i %i]\n%s',x,y,z,viewGet(v,'description',viewGet(v,'curScan')));
+  tSeries = squeeze(tSeries);
+  tSeriesSte = nan(1,length(tSeries));
 end
 
-tseries = tseries(junkframes+1:junkframes+nframes);
+% get the time series
+t = linspace(framePeriod/2,(length(tSeries)-.5)*framePeriod,length(tSeries))';
 
-%recalculate CorAnal (not necessary for single voxels, but greatly simplifies the code)
-[coval, ampval, phval, ptseries] = computeCoranal(tseries,ncycles,detrend,spatialnorm,trigonometricFunction);
- 
+% reomove junk frames and adjust length (if needed)
+tSeries = tSeries(junkframes+1:junkframes+nframes);
+tSeriesSte = tSeriesSte(junkframes+1:junkframes+nframes);
+t = t(junkframes+1:junkframes+nframes);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%
+%% computeCorAnalFit
+%%%%%%%%%%%%%%%%%%%%%%%
+function [corAnalFit, singleCycle, singleCycleSte] = computeCoranalFit(tSeries, nCycles, coVal, ampVal, phVal,trigonometricFunction)
+
+% default return values
+corAnalFit = [];
+corAnalSingleCycleFit = [];
+singleCycle = [];
+singleCycleSte = [];
+
+% get length of tSeries
+nFrames = length(tSeries);
+
 % calculate mean cycle
-if rem(nframes,ncycles)
-  disp(sprintf('(corAnalPlot) Scan has %i frames, not evenly divisible by %i cycles',nframes,ncycles));
+if rem(nFrames,nCycles)
+  disp(sprintf('(corAnalPlot) Scan has %i frames, not evenly divisible by %i cycles',nFrames,nCycles));
+  % chop off the tSeries at the end. Could so something
+  % fancier and add the partial cycle back into the man and ajdust
+  % the ste for the number of points, but seems like overkill.
+  nFrames = floor(nFrames/nCycles)*nCycles;
 end
-%return
-cycleLen = floor(nframes/ncycles);
-singleCycleTSeries = ptseries(1:cycleLen*ncycles);
-singleCycle = mean(reshape(singleCycleTSeries,cycleLen,ncycles)');
-singleCycleSte = std(reshape(singleCycleTSeries,cycleLen,ncycles)')/sqrt(ncycles);
 
-% calculate fft
-ft = fft(ptseries);
-absft = abs(ft(1:round((length(ft)/2))));
-signalAmp = absft(ncycles+1);
-noiseFreq = round(2*length(absft)/3):length(absft);
-noiseAmp = mean(absft(noiseFreq));
-snr = signalAmp/noiseAmp;
+% compute the single cycle and ste
+cycleLen = floor(nFrames/nCycles);
+singleCycleTSeries = tSeries(1:nFrames);
+singleCycle = mean(reshape(singleCycleTSeries,cycleLen,nCycles)');
+singleCycleSte = std(reshape(singleCycleTSeries,cycleLen,nCycles)')/sqrt(nCycles);
+
+% normalize to percent signal change with 0 mean
+singleCycle = 100*(singleCycle-mean(singleCycle));
+singleCycleSte = 100*singleCycleSte;
 
 % Create model fit
 switch (trigonometricFunction)
   case 'Cosine'
-    model = ampval * cos(2*pi*ncycles/nframes * [0:nframes-1]' - phval);
+    corAnalFit = ampVal * cos(2*pi*nCycles/nFrames * [0:nFrames-1]' - phVal);
   case 'Sine'
-    model = ampval * sin(2*pi*ncycles/nframes * [0:nframes-1]' - phval);
+    corAnalFit = ampVal * sin(2*pi*nCycles/nFrames * [0:nFrames-1]' - phVal);
 end
 
-% Change xlabel to be in seconds
-% Change ylabel contingent upon detrend to include units
+%%%%%%%%%%%%%%%%%%%%%%
+% plot the tSeries
+%%%%%%%%%%%%%%%%%%%%%%
+function emriPlotTSeries(t, tSeries, tSeriesSte, absft, nRows, nCols, headerStr)
+
+% plot settings
+markerSize = 12;
+fontSize = 14;
 
 % Select window
 selectGraphWin;
 
-% Plot it
-subplot(2,3,1:3)
-time = linspace(framePeriod/2,(nframes-.5)*framePeriod,nframes)';
-if roiPlot
-    errorbar(time,ptseries,ptseriesSte,'k.-','LineWidth',1);hold on
+% Plot timeSeries
+subplot(nRows,nCols,1:nCols)
+if all(isnan(tSeriesSte))
+  plot(t,100*(tSeries-1),'k.-','LineWidth',1,'MarkerSize',markerSize);hold on
 else
-    plot(time,ptseries,'k.-','LineWidth',1);hold on
+  errorbar(t,100*(tSeries-1),100*tSeriesSte,'k.-','LineWidth',1,'MarkerSize',markerSize);hold on
 end
-plot(time,model,'r-','LineWidth',1.5);
-
-% Ticks and labels
-fontSize = 14;
-set(gca,'FontSize',fontSize);
-headerStr = sprintf('%s freq=%i/scan co=%f amp=%f ph(%s)=%f (%i deg)\n%s',...
-  headerStr,ncycles,coval,ampval,trigonometricFunction,phval,round(180*phval/pi),viewGet(v,'description',viewGet(v,'curScan')));
-title(headerStr,'Interpreter','none');
-xtickStep = nframes*framePeriod/ncycles;
-xtick = ceil([0:xtickStep:nframes*framePeriod]);
-xtick = sort(unique(xtick));
-set(gca,'xtick',xtick);
-set(gca,'XLim',ceil([0,nframes*framePeriod]));
+title(headerStr);
+xlabel('Time (s)');
+ylabel('Response amplitude (% signal change)');
 set(gca,'xgrid','on')
-xlabel('Time (sec)');
-switch spatialnorm
-    case {'Divide by mean'}
-        ylabel('fMRI response (% change in image intensity)');
-    otherwise
-        ylabel('fMRI response(arbitrary units)');
-end
-
-
-% Plot single cylce
-subplot(2,3,4)
-time = linspace(framePeriod/2,(nframes/ncycles-.5)*framePeriod,nframes/ncycles)';
-errorbar(time,singleCycle,singleCycleSte,'k.-','LineWidth',1);hold on
-plot(time,model(1:length(time)),'r-','LineWidth',1.5);
-
+set(gca,'XLim',[min(t) max(t)]);
 set(gca,'FontSize',fontSize);
-title('Single cycle with STE across cycles');
-xlabel('Time (sec)');
-switch spatialnorm
-    case {'Divide by mean'}
-        ylabel('fMRI response (% change in image intensity)');
-    otherwise
-        ylabel('fMRI response(arbitrary units)');
-end
 
-% plot fourier amplitude
-subplot(2,3,5:6)
-plot(0:(length(absft)-1),absft,'k.-');hold on
-plot(ncycles,signalAmp,'ro');
-plot(noiseFreq-1,absft(noiseFreq),'go');
+% Plot Fourier Transform
+subplot(nRows,nCols,nCols+1:2*nCols)
+plot(0:(length(absft)-1),absft,'k.-','MarkerSize',markerSize);hold on
 set(gca,'FontSize',fontSize);
-title(sprintf('Stimulus (red): %f Noise (Mean of green): %f CNR: %f',signalAmp,noiseAmp,snr));
 ylabel('Magnitude');
 xlabel('Fourier component number');
+set(gca,'XLim',[0 (length(absft)-1)]);
 
-return
 
+%%%%%%%%%%%%%%%%%%%%%%
+% plot corAnal
+%%%%%%%%%%%%%%%%%%%%%%
+function emriPlotCorAnal(t, tSeries, tSeriesSte, absft, nCycles, coVal, ampVal, phVal, trigonometricFunction, nRows, nCols, headerStr)
+
+% plot se
+markerSize = 12;
+fontSize = 14;
+
+% compute cor anal model fits
+[corAnalFit, singleCycle, singleCycleSte] = computeCoranalFit(tSeries, nCycles, coVal, ampVal, phVal, trigonometricFunction);
+
+% calculate SNR as the signal at the analysis frequency divided by the
+% mean at the top 1/3 of high frequencies (as an estimate of noise)
+signalAmp = absft(nCycles+1);
+noiseFreq = round(2*length(absft)/3):length(absft);
+noiseAmp = mean(absft(noiseFreq));
+snr = signalAmp/noiseAmp;
+
+% Select window
+selectGraphWin(true);
+
+% Set title for time series
+subplot(nRows,nCols,1:nCols);
+headerStr = sprintf('%s\nfreq=%i cycles/scan (%0.1f cycles/sec) co=%f amp=%f ph(%s)=%f (%i deg)',...
+  headerStr,nCycles,nCycles*t(end),coVal,ampVal,trigonometricFunction,phVal,round(180*phVal/pi));
+title(headerStr,'Interpreter','none');
+
+% put the ticks upon each cycle
+xtickStep = t(length(singleCycle))-t(1);
+xtick = [0:xtickStep:t(end)];
+xtick = sort(unique(xtick));
+set(gca,'xtick',xtick);
+
+% Plot model fit
+hold on; plot(t(1:length(corAnalFit)),corAnalFit,'r-','LineWidth',1);
+
+% Plot single cylce
+subplot(nRows,nCols,nCols+1)
+myerrorbar(t(1:length(singleCycle)),singleCycle,'yError',singleCycleSte,'LineWidth',1,'MarkerSize',markerSize);hold on
+plot(t(1:length(singleCycle)),corAnalFit(1:length(singleCycle)),'r-','LineWidth',1.5);
+set(gca,'XLim',[t(1) t(length(singleCycle))]);
+
+% labels
+set(gca,'FontSize',fontSize);
+title('Single cycle with STE across cycles');
+xlabel('Time (s)');
+ylabel('Response amplitude (% signal change)');
+
+% Plot fft
+subplot(nRows,nCols,nCols+2:nCols*2)
+plot(0:(length(absft)-1),absft,'k.-','MarkerSize',markerSize);hold on
+set(gca,'FontSize',fontSize);
+ylabel('Magnitude');
+xlabel('Fourier component number');
+plot(nCycles,signalAmp,'ro');
+plot(noiseFreq-1,absft(noiseFreq),'go');
+title(sprintf('Stimulus (red): %f Noise (Mean of green): %f CNR: %f',signalAmp,noiseAmp,snr));
+set(gca,'XLim',[0 (length(absft)-1)]);
 
